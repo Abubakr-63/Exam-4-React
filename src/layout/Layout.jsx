ce801657aec18467daa471c9a4b82fcfb8485ade
Abubakr <kurbonovabubakr800@gmail.com>
2026-09-02 22:39:46 +0500

    changed problem

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Outlet } from 'react-router'
-import Header from '../components/Header'
+import Header from '../components/Header.jsx'
 import Footer from '../components/Footer'
 
 export default function Layout() {
