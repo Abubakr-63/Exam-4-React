e2aeb0bc6b986bc1f1e013e1a3f84bf4e1fdd88a
Abubakr <kurbonovabubakr800@gmail.com>
2026-09-02 22:16:07 +0500

    fixed problem

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from 'react'
 import { Outlet } from 'react-router'
-import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 export default function Layout() {
   const [count, setCount] = useState(0)
